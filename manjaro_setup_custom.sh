73d23deac7e8501fb798d4b0c0d5b1ba4fca971e
Hrotkó Gábor <hrotkogabor@gmail.com>
2021-03-23 23:44:16 +0100

    fix vivaldi

--- manjaro_setup_custom.sh
+++ manjaro_setup_custom.sh
@@ -9,6 +9,8 @@ fi
 # gwenview imageviewer, with imageformats and video thumbnails, image rating will not work with all theme
 echo "1" | sudo pacman -S --needed --noconfirm gwenview kio-extras qt5-imageformats kimageformats ffmpegthumbs byobu
 
+yes | LC_ALL=en_US.UTF-8 sudo pacman -Syyuu vivaldi
+
 # install some stuff from AUR
 export GNUPGHOME=~/aur
 yay -aS --sudoloop --noredownload --norebuild --noconfirm --noeditmenu youtube-dl-gui-git yad-git pdfchain
@@ -16,9 +18,6 @@ yay -aS --sudoloop --noredownload --norebuild --noconfirm --noeditmenu youtube-d
 if [ $(uname -m) = "x86_64" ]; then
 	export GNUPGHOME=~/aur; yay -aS --sudoloop --noredownload --noconfirm --noeditmenu gnome-encfs-manager-bin
 fi
-
-# vivaldi
-yay -aS --sudoloop --noredownload --norebuild --noconfirm --noeditmenu vivaldi
 
 # install codecs
 sudo /opt/vivaldi/update-ffmpeg
