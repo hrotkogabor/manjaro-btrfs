def5fbd14725e186f9b70a9ca31e2181bf5b733e
Hrotkó Gábor <hrotkogabor@gmail.com>
2021-10-12 16:24:15 +0200

    fix vivaldi ffmpeg-codecs

--- manjaro_setup_custom.sh
+++ manjaro_setup_custom.sh
@@ -9,20 +9,15 @@ fi
 # gwenview imageviewer, with imageformats and video thumbnails, image rating will not work with all theme
 echo "1" | sudo pacman -S --needed --noconfirm gwenview kio-extras qt5-imageformats kimageformats ffmpegthumbs byobu
 
-yes | LC_ALL=en_US.UTF-8 sudo pacman -Syyuu vivaldi
+yes | LC_ALL=en_US.UTF-8 sudo pacman -Syyuu vivaldi vivaldi-ffmpeg-codecs
 
 # install some stuff from AUR
 export GNUPGHOME=~/aur
-yay -aS --sudoloop --noredownload --norebuild --noconfirm --noeditmenu youtube-dl-gui-git yad-git pdfchain
+yay -aS --sudoloop --noredownload --norebuild --noconfirm --noeditmenu youtube-dl-gui-git yad pdfchain
 
 if [ $(uname -m) = "x86_64" ]; then
 	export GNUPGHOME=~/aur; yay -aS --sudoloop --noredownload --noconfirm --noeditmenu gnome-encfs-manager-bin
 fi
-
-# install codecs
-sudo /opt/vivaldi/update-ffmpeg
-# drm
-sudo /opt/vivaldi/update-widevine
 
 echo -e "\nInstall custom stuff ready."
 
