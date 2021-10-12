bea8615a55c91fef728aa9d5f7674a3ac225e262
Hrotkó Gábor <hrotkogabor@gmail.com>
2021-10-12 16:23:46 +0200

    - create mirror list only with continent - remove timeshift, as we use snapper - add system-config-printer and downgrade - disable core dump

--- install/script/stage_2_setup.sh
+++ install/script/stage_2_setup.sh
@@ -24,7 +24,9 @@ echo -e "\nBlacklisting mirror from Iran"
 # https://forum.manjaro.org/t/pacman-mirrors-freezes-on-iran/79700/15
 echo "127.0.0.1    repo.sadjad.ac.ir" | sudo tee -a /etc/hosts
 #sudo pacman-mirrors --country Germany,France,Austria
-sudo pacman-mirrors --fasttrack --timeout 2 -m rank
+#sudo pacman-mirrors --fasttrack --timeout 2 -m rank
+sudo pacman-mirrors --continent --timeout 2 -m rank
+
 
 # for 32 bit we need to update keys with this
 # https://forum.manjaro.org/t/solved-manjaro-32-refuses-updates-no-matter-what/68522/2
@@ -42,9 +44,12 @@ fi
 # do full upgrade
 sudo pacman -Syyuu --needed --noconfirm 
 
+# remove timeshift, as we do it with snapper
+yes | LC_ALL=en_US.UTF-8 sudo pacman -R timeshift-autosnap-manjaro timeshift
+
 #install default stuff
 yes | LC_ALL=en_US.UTF-8 sudo pacman -S --needed --noconfirm base-devel
-yes | LC_ALL=en_US.UTF-8 sudo pacman -S --needed --overwrite /etc/skel/.config/autostart binutils mc mtools unarj activity-log-manager at baobab exempi caja caja-extensions-common caja-share caja-sendto caja-wallpaper caja-open-terminal caja-xattr-tags caja-image-converter catdoc copyq doublecmd-gtk2 libunrar dstat gnome-disk-utility gnome-system-monitor hardinfo hddtemp iotop keepassxc lshw meld gnome-nettool seahorse smplayer smtube strace youtube-dl zeitgeist-explorer yay synapse fd manjaro-tools-base lbzip2 compsize filemanager-actions pigz gnome-calculator
+yes | LC_ALL=en_US.UTF-8 sudo pacman -S --needed --overwrite /etc/skel/.config/autostart binutils mc mtools unarj activity-log-manager at baobab exempi caja caja-extensions-common caja-share caja-sendto caja-wallpaper caja-open-terminal caja-xattr-tags caja-image-converter catdoc copyq doublecmd-gtk2 libunrar dstat gnome-disk-utility gnome-system-monitor hardinfo hddtemp iotop keepassxc lshw meld gnome-nettool seahorse smplayer smtube strace youtube-dl zeitgeist-explorer yay synapse fd manjaro-tools-base lbzip2 compsize filemanager-actions pigz gnome-calculator system-config-printer downgrade
 
 # synapse vs ulauncher?
 
@@ -125,6 +130,11 @@ sudo sed -i -e 's/PRUNENAMES = "/PRUNENAMES = ".snapshots /g' /etc/updatedb.conf
 yes | LC_ALL=en_US.UTF-8 sudo pacman -S --needed --noconfirm snap-pac grub-btrfs snapper-gui
 sudo sed -i -e 's/#GRUB_BTRFS_SUBMENUNAME="Arch Linux snapshots"/GRUB_BTRFS_SUBMENUNAME="Select snapshot"/g' /etc/default/grub-btrfs/config
 
+# disable core dump
+sudo mkdir -p /etc/systemd/coredump.conf.d
+sudo touch /etc/systemd/coredump.conf.d/custom.conf
+echo -e "[Coredump]\nStorage=none" | sudo tee /etc/systemd/coredump.conf.d/custom.conf
+sudo systemctl daemon-reload
 
 # run btrfs read-only check after login
 echo '[Desktop Entry]
