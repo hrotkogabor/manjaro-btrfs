b10e49ac8346be295573bec5a6cd3552f9bb6a8b
Hrotkó Gábor <hrotkogabor@gmail.com>
2022-03-31 17:18:46 +0200

    Update manjaro_setup_keyfix.sh

--- manjaro_setup_keyfix.sh
+++ manjaro_setup_keyfix.sh
@@ -8,15 +8,19 @@ sudo rm -fr /etc/pacman.d/gnupg
 yes | LC_ALL=en_US.UTF-8 sudo pacman-key --init
 
 if [ $(uname -m) = "i686" ]; then
-	yes | LC_ALL=en_US.UTF-8 sudo pacman -S archlinux32-keyring-transition
 	#yes | LC_ALL=en_US.UTF-8 sudo pacman -Sy archlinux32-keyring manjaro-keyring
 	yes | LC_ALL=en_US.UTF-8 sudo pacman-key --populate archlinux32 manjaro
+	yes | LC_ALL=en_US.UTF-8 sudo pacman -S archlinux32-keyring-transition
 else
-	yes | LC_ALL=en_US.UTF-8 sudo pacman -Sy archlinux-keyring manjaro-keyring
 	yes | LC_ALL=en_US.UTF-8 sudo pacman-key --populate archlinux manjaro
+	yes | LC_ALL=en_US.UTF-8 sudo pacman -Sy archlinux-keyring manjaro-keyring
 fi
 
 yes | LC_ALL=en_US.UTF-8 sudo pacman-key --refresh-keys
-yes | LC_ALL=en_US.UTF-8 sudo pacman -Syyuu
+
+# alternativly
+#yes | LC_ALL=en_US.UTF-8 sudo pacman-key --keyserver hkps://sks.pod02.fleetstreetops.com --refresh-keys
+
+yes | LC_ALL=en_US.UTF-8 sudo pacman -Syyuu --needed --noconfirm
 
 read
